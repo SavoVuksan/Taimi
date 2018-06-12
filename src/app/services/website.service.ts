5aed7783d3ca00b309b90db3f102f7f889e02458
Savo Vuksan <savovuksan@gmail.com>
2018-06-12 12:02:29 +0200

    Added Website Blocking feature via Windows Host Files

--- src/app/services/website.service.ts
+++ src/app/services/website.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {DatabaseService} from './database.service';
 import {Website} from '../classes/website';
 
+const fs = require('fs');
+const hostFilePath = 'C:\\Windows\\System32\\drivers\\etc\\hosts';
+
 @Injectable()
 export class WebsiteService {
 
@@ -21,7 +24,38 @@ export class WebsiteService {
 
   updateWebsite(website: Website){
     this.database.updateWebsite(website);
+    this.updateHostFile();
+
   }
+
+  updateHostFile(){
+    // Get Current state of Host file
+    let hostFileContent = fs.readFileSync(hostFilePath,'UTF-8');
+
+    // Backup host file
+    fs.exists(hostFilePath+'_backup', (exists) => {
+      console.log(`Backup file ${(exists === true ? 'exists' : 'doesnt exist')} `);
+      if(!exists){
+        fs.writeFileSync(hostFilePath+'_backup',hostFileContent,{encoding: 'UTF-8'});
+
+      }else{
+        hostFileContent = fs.readFileSync(hostFilePath+'_backup','UTF-8');
+      }
+    });
+
+    setTimeout(() => {
+      //Get all Websites which have to get redirected
+      for(let w of this.websites){
+        if(w.blocked){
+          hostFileContent += `\n 127.0.0.1 ${w.name}`;
+        }
+      }
+      fs.writeFileSync(hostFilePath,hostFileContent,{encoding: 'UTF-8'});
+
+    },700);
+
+  }
+
   deleteWebsite(website: Website){
     this.database.deleteWebsite(website);
     const index = this.websites.indexOf(website,0);
